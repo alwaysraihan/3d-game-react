183911eda6b081b581dcf243c9e6965cc8176a4d
alwaysraihan <bdyoungdeveloper@gmail.com>
2022-11-16 13:16:18 +0600

    initial setup

---
+++ src/components/Scene/Scene.tsx
@@ -0,0 +1,20 @@
+
+const Scene = () => {
+  return (
+    <div>
+
+        <h1>Scene</h1>
+        <h1>Scene</h1>
+        <h1>Scene</h1>
+        <h1>Scene</h1>
+        <h1>Scene</h1>
+        <h1>Scene</h1>
+        <h1>Scene</h1>
+        <h1>Scene</h1>
+        <h1>Scene</h1>
+        <h1>Scene</h1>
+    </div>
+  )
+}
+
+export default Scene
---
+++ src/screens/Home.tsx
@@ -0,0 +1,13 @@
+import { Canvas } from '@react-three/fiber'
+import Scene from '../components/Scene/Scene'
+const Home = () => {
+  return (
+    <>
+<Canvas>
+  <Scene/>
+</Canvas>    
+    </>
+  )
+}
+
+export default Home
